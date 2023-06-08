ddea7e9ebe4e73addef4e59d8f2ffc7f16baed83
He Mingfei <hemingfei@live.com>
2023-06-08 16:25:02 +0800

    add show each sprite size in config editor widow

--- Assets/Framework/CustomGameFramework/Editor/SpriteAtlas/SpriteAtlasUserConfigEditor.cs
+++ Assets/Framework/CustomGameFramework/Editor/SpriteAtlas/SpriteAtlasUserConfigEditor.cs
@@ -6,6 +6,7 @@
 *	功能：暂无
 *****************************************************/
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -18,9 +19,21 @@ namespace CustomGameFramework.Editor.SpriteAtlas
     [CustomEditor(typeof(SpriteAtlasUserConfig), false)]
     public class SpriteAtlasUserConfigEditor : UnityEditor.Editor
     {
+        private float _pngSize = 0;
+        private float _pngCount = 0;
+        private float _textureSize = 0;
+        
+        private Dictionary<string, Dictionary<string, string>> _spriteAtlasSizeData = new Dictionary<string, Dictionary<string, string>>();
+
+        public void OnEnable()
+        {
+            PreCheckAsset();
+        }
+
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
+
             if (GUILayout.Button("修改保存"))
             {
                 EditorUtility.SetDirty(target);
@@ -34,12 +47,93 @@ namespace CustomGameFramework.Editor.SpriteAtlas
                     GUIUtility.hotControl = 0;
                 }
             }
+            
             GUILayout.Space(30);
             GUILayout.Label("使用说明");
-            GUILayout.Label("\n1. 将图片文件夹拖入上方" +
-                            "\n2. 工具会自动遍历文件夹里包含的子文件夹" +
-                            "\n3. 每一个[最终的子文件夹]会被打成一个图集" +
-                            "\n\n注意： 若一个文件夹既包含子文件夹又包含图片，\n            那么只有子文件夹会被打图集，\n            该文件夹和里面的图片不会被打图集");
+            GUILayout.Label("\n1. 将图片文件夹拖入" +
+                            "\n2. 工具会自动遍历文件夹里的图片" +
+                            "\n3. 每一个 SpriteAtlasUserConfig 会被打成一个图集");
+            GUILayout.Space(30);
+            
+            GUILayout.Space(30);
+            GUILayout.Label("图片个数：" + _pngCount);
+            GUILayout.Label("源文件大小：" + _pngSize + "MB");
+            GUILayout.Label("Texture大小：" + _textureSize + "MB");
+            if (GUILayout.Button("检测图集资源"))
+            {
+                PreCheckAsset();
+            }
+            // 显示嵌套的字典
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("File", GUILayout.Width(100));
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("Texture Size", GUILayout.ExpandWidth(true));
+            GUILayout.EndHorizontal();
+
+            foreach (string key in _spriteAtlasSizeData.Keys)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(key);
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+
+                foreach (string innerKey in _spriteAtlasSizeData[key].Keys)
+                {
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Space(20);
+                    GUILayout.Label(innerKey);
+                    GUILayout.FlexibleSpace();
+                    GUILayout.Label(_spriteAtlasSizeData[key][innerKey]);
+                    GUILayout.EndHorizontal();
+                }
+            }
+            GUILayout.Space(30);
+        }
+
+        public void PreCheckAsset()
+        {
+            var folders = ((SpriteAtlasUserConfig)target).SpriteFolders.Select(AssetDatabase.GetAssetPath).ToArray();
+            int totalCount = 0;
+            long totalSize = 0;
+            int totalTextureSize = 0;
+            _spriteAtlasSizeData = new Dictionary<string, Dictionary<string, string>>();
+            foreach (string folder in folders)
+            {
+                string folderName = folder.Split('/').Last();
+                _spriteAtlasSizeData.Add(folderName, new Dictionary<string, string>());
+            }
+            foreach (string folder in folders)
+            {
+                if (Directory.Exists(folder))
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(folder);
+                    FileInfo[] files = directoryInfo.GetFiles("*.png", SearchOption.AllDirectories);
+                    string folderName = folder.Split('/').Last();
+                    string folderPre = folder.Replace(folderName, "");
+                    foreach (var file in files)
+                    {
+                        totalCount++;
+                        totalSize += file.Length;
+                        
+                        string assetPath = file.FullName.Replace("\\", "/").Replace(Application.dataPath, "Assets");
+                        TextureImporter importer = AssetImporter.GetAtPath(assetPath) as TextureImporter;
+                        if (importer != null)
+                        {
+                            int width = 0;
+                            int height = 0;
+                            importer.GetSourceTextureWidthAndHeight(out width, out height);
+                            int size = width * height;
+                            totalTextureSize += size;
+                            _spriteAtlasSizeData[folderName].Add(assetPath.Replace(folderPre, ""), (size/ 1024f).ToString("F2") + "KB");
+                        }
+                    }
+                }
+            }
+            float sizeMB = totalSize / 1048576f;
+
+            _pngCount = totalCount;
+            _pngSize = sizeMB;
+            _textureSize = totalTextureSize / 1048576f;
         }
     }
 }
